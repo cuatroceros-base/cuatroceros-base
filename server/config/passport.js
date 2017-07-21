622acd04d8633c019196d7f64fa147290c692221
Jorge Pedrejon <jgpedrejon@gmail.com>
2017-07-21 13:17:52 +0200

    beitify

--- server/config/passport.js
+++ server/config/passport.js
@@ -2,29 +2,24 @@ var LocalStrategy = require('passport-local').Strategy
 var User = require('../models/User')
 const bcrypt = require('bcrypt')
 
-module.exports = function (passport) {
+module.exports = function(passport) {
   passport.use(new LocalStrategy((email, password, next) => {
     User.findOne({
-      email
-    }, (err, user) => {
-      if (err) {
-        return next(err)
-      }
-
-      if (!user) {
-        return next(null, false, {
-          message: 'Incorrect email'
-        })
-      }
-
-      if (!bcrypt.compareSync(password, user.password)) {
-        return next(null, false, {
-          message: 'Incorrect password'
-        })
-      }
-
-      return next(null, user)
-    })
+        email
+      }).exec()
+      .then((user) => {
+        if (!user) {
+          return next(null, false, {
+            message: 'Incorrect email'
+          })
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+          return next(null, false, {
+            message: 'Incorrect password'
+          })
+        }
+        return next(null, user)
+      }).catch(err => next(err))
   }))
 
   passport.serializeUser((user, cb) => {
